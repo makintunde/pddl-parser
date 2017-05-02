af8e22bf3b2832a81f9a2961a7704888d6e43006
ma4413 <michael.akintunde13@imperial.ac.uk>
2017-05-02 22:38:54 +0100

    Separate environment from agent

--- code_generator.py
+++ code_generator.py
@@ -8,7 +8,8 @@ class CodeGenerator:
         self.parser = parser
         self.code_generator = []
         self.indentation_depth = 4
-        self.agent_name = 'action'
+        self.action_performing_agent = 'Performer'
+        self.environment = 'Environment'
         self.variable_map = {}
         self.effects = {}
         self.combinations = {}
@@ -17,14 +18,22 @@ class CodeGenerator:
         tabs = ' ' * (depth * self.indentation_depth)
         self.code_generator.append(tabs + line)
 
-    def add_agent(self):
-        self.add_line(0, 'Agent ' + self.agent_name)
-        self.add_vars()
-        self.add_redstates()
+    def add_environment_agent(self):
+        self.add_line(0, 'Agent ' + self.environment)
+        self.add_vars(obs=True)
+        self.add_red_states()
+        self.add_actions(empty=True)
+        self.add_protocol(empty=True)
+        self.add_evolution()
+        self.add_line(0, 'end Agent')
+
+    def add_action_performing_agent(self):
+        self.add_line(0, 'Agent ' + self.action_performing_agent)
+        self.add_vars(empty=True)
         self.prepare_actions()
         self.add_actions()
         self.add_protocol()
-        self.add_evolution()
+        self.add_evolution(empty=True)
         self.add_line(0, 'end Agent')
 
     def initialise_variable_map(self):
@@ -32,8 +41,12 @@ class CodeGenerator:
             variable = '_'.join(s).replace('-', '')
             self.variable_map[variable] = True
 
-    def add_vars(self):
-        self.add_line(1, 'Vars:')
+    def add_vars(self, obs=None, empty=None):
+        self.add_line(1, ('Vars' if obs is None else 'Obsvars') + ':')
+        if empty is not None:
+            self.add_line(2, 'state : { empty };')
+            self.add_line(1, 'end ' + ('Vars' if obs is None else 'Obsvars'))
+            return
         var_type = 'boolean'
         predicates = self.parser.predicates
         objects = self.parser.objects
@@ -48,9 +61,9 @@ class CodeGenerator:
                     self.variable_map[j.replace('-', '')] = False
                 self.add_line(2, j + ' : ' + var_type + ';')
             
-        self.add_line(1, 'end Vars')
+        self.add_line(1, 'end ' + ('Vars' if obs is None else 'Obsvars'))
     
-    def add_redstates(self):
+    def add_red_states(self):
         self.add_line(1, 'RedStates:')
         self.add_line(1, 'end RedStates')
 
@@ -66,7 +79,7 @@ class CodeGenerator:
 
                 for precondition in action.positive_preconditions:
                     candidate = self.get_candidate(comb, param_map, precondition)
-                    candidates.append(candidate)
+                    candidates.append('Environment.' + candidate)
 
                 for positive in action.add_effects:
                     candidate = self.get_candidate(comb, param_map, positive)
@@ -92,38 +105,51 @@ class CodeGenerator:
         candidate = '_'.join([predicate] + arguments)
         return candidate
 
-    def add_actions(self):
+    def add_actions(self, empty=None):
         self.add_line(1, 'Actions = {')
-        self.add_line(2, ', '.join(action for action, _ in self.effects.items()))
+        if empty is None:
+            self.add_line(2, ', '.join(action for action, _ in self.effects.items()))
+        else:
+            self.add_line(2, 'none')
         self.add_line(1, '};')
 
-    def add_protocol(self):
+    def add_protocol(self, empty=None):
+        self.add_line(1, 'Protocol:')
+
+        if empty is not None:
+            self.add_line(2, 'Other : { none };')
+            self.add_line(1, 'end Protocol')
+            return
+
         next_actions = collections.defaultdict(set)
 
         for action_name, next_combination in self.combinations.items():
             next_actions[next_combination].add(action_name)
 
-        self.add_line(1, 'Protocol:')
         for precondition, actions in next_actions.items():
             next_str = precondition + ' : { ' + ', '.join(actions) + ' };'
             self.add_line(2, next_str)
+
         self.add_line(1, 'end Protocol')
 
-    def add_evolution(self):
+    def add_evolution(self, empty=None):
         self.add_line(1, 'Evolution:')
-        for action, effect in self.effects.items():
-            next_line = effect + ' if Action=' + action + ';'
-            self.add_line(2, next_line)
+        if empty is None:
+            for action, effect in self.effects.items():
+                next_line = effect + ' if ' + self.action_performing_agent + '.Action=' + action + ';'
+                self.add_line(2, next_line)
+        else:
+            self.add_line(2, 'state=empty if state=empty;')
         self.add_line(1, 'end Evolution')
 
-    def add_evaluation(self):
+    def add_evaluation(self, agent_name):
         self.add_line(0, 'Evaluation')
 
         goals = []
-        positive_goals = ['action.' + '_'.join(goal) + '=true' for goal in self.parser.positive_goals]
+        positive_goals = [agent_name + '.' + '_'.join(goal) + '=true' for goal in self.parser.positive_goals]
         positive_goal_spec = ' and '.join(positive_goals)
-        
-        negative_goals = ['action.' + '_'.join(goal) + '=false' for goal in self.parser.negative_goals]
+
+        negative_goals = [agent_name + '.' + '_'.join(goal) + '=false' for goal in self.parser.negative_goals]
         negative_goal_spec = ' and '.join(negative_goals) 
 
         if positive_goals:
@@ -136,19 +162,19 @@ class CodeGenerator:
         self.add_line(1, 'goal if ' + goal_spec + ';')
         self.add_line(0, 'end Evaluation')
 
-    def add_init_states(self):
+    def add_init_states(self, agent_name):
         self.add_line(0, 'InitStates')
         init_states = []
         for variable in self.variable_map:
             truth_strings = ['false', 'true']
             truth = truth_strings[self.variable_map[variable]]
-            init_states.append(self.agent_name + '.' + variable + '=' + truth)
+            init_states.append(agent_name + '.' + variable + '=' + truth)
         self.add_line(1, ' and\n    '.join(init_states) + ';')
         self.add_line(0, 'end InitStates')
 
-    def add_groups(self):
+    def add_groups(self, agents):
         self.add_line(0, 'Groups')
-        self.add_line(1, 'g1 = { ' + self.agent_name + ' };')
+        self.add_line(1, 'g1 = { ' + ', '.join(agents) + ' };')
         self.add_line(0, 'end Groups')
 
     def add_fairness(self):
@@ -162,10 +188,12 @@ class CodeGenerator:
 
     def generate(self):
         self.initialise_variable_map()
-        self.add_agent()
-        self.add_evaluation()
-        self.add_init_states()
-        self.add_groups()
+        self.prepare_actions()
+        self.add_environment_agent()
+        self.add_action_performing_agent()
+        self.add_evaluation(self.environment)
+        self.add_init_states(self.environment)
+        self.add_groups([self.action_performing_agent])
         self.add_fairness()
         self.add_formulae()
     
